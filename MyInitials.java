9b98ed172767136206a913942349aaacbb8e1992
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-11-10 20:54:43 -0600

    Print my name

--- MyInitials.java
+++ MyInitials.java
@@ -1,11 +1,11 @@
 public class MyInitials {
     public static void main(String[] args){
-        System.out.println("KKKKKAAAAA");
-        System.out.println("KKKKKAAAAA");
-        System.out.println("KKKKKAAAAA");
-        System.out.println("KKKKKAAAAA");
-        System.out.println("KKKKKAAAAA");
-        System.out.println("KKKKKAAAAA");
-        System.out.println("KKKKKAAAAA");
+        System.out.println("K   K    A  ");
+        System.out.println("K  K    A A ");
+        System.out.println("K K    A   A");
+        System.out.println("KK     AAAAA");
+        System.out.println("K K    A   A");
+        System.out.println("K  K   A   A");
+        System.out.println("K   K  A   A");
     }
 }
