70fe00082212b01de6c092f4065272e2b67648e0
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-11-10 22:38:30 -0600

    Bus Tickets Count update

--- BusTickets.java
+++ BusTickets.java
@@ -1,11 +1,14 @@
 public  class BusTickets {
     public static void main(String[] args) {
+        String sentence = "Number of passengers: ";
         int passenger = 0;
         passenger += 9;
-        System.out.println(passenger);
+        System.out.println(sentence + passenger);
         passenger -= 5;
-        System.out.println(passenger);
+        System.out.println(sentence + passenger);
         passenger = passenger - 4;
-        System.out.println(passenger);
+        System.out.println(sentence + passenger);
+        sentence = "end of shift";
+        System.out.println(sentence);
     }
 }
