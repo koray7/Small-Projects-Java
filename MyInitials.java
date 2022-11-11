cd829577ac9d38991c60d9aab505ab47a7ae9a93
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-11-10 20:42:59 -0600

    Print names

---
+++ MyInitials.java
@@ -0,0 +1,11 @@
+public class MyInitials {
+    public static void main(String[] args){
+        System.out.println("KKKKKAAAAA");
+        System.out.println("KKKKKAAAAA");
+        System.out.println("KKKKKAAAAA");
+        System.out.println("KKKKKAAAAA");
+        System.out.println("KKKKKAAAAA");
+        System.out.println("KKKKKAAAAA");
+        System.out.println("KKKKKAAAAA");
+    }
+}
