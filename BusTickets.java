2933b62596409c0852724bb02ab12304ccd7675c
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-11-10 22:15:58 -0600

    Bus Tickets Count

---
+++ BusTickets.java
@@ -0,0 +1,11 @@
+public  class BusTickets {
+    public static void main(String[] args) {
+        int passenger = 0;
+        passenger += 9;
+        System.out.println(passenger);
+        passenger -= 5;
+        System.out.println(passenger);
+        passenger = passenger - 4;
+        System.out.println(passenger);
+    }
+}
