3bf10bfcbc9db56a422ef03d93afd6375ccd3089
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-11-12 13:33:41 -0600

    My Bio

---
+++ MyBio.java
@@ -0,0 +1,16 @@
+public class MyBio {
+    public static void main(String[] args) {
+        String name = "Koray";
+        int age = 30;
+        String country = "United States";
+        String sport = "Soccer";
+        int hours = 45;
+        String game = "Clash of Clans";
+        String lesson = "function";
+        char grade = 'A';
+        System.out.println("My name is " + name + ". I'm " + age + " years old, and I'm from " + country + ".");
+        System.out.println("My favorite sport is " + sport + " and I always try to play on my free time. I work " + hours + " hours a week. ");
+        System.out.println("When I'm tired, I like to play " + game);
+        System.out.println("In school, my favorite lesson was " + lesson + ", I scored an " + grade + ".");
+    }
+}
