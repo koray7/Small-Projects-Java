8d2ba067d494ab3456019044463847206c85c599
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-11-15 23:57:50 -0600

    If Else statements

---
+++ IfElse.java
@@ -0,0 +1,32 @@
+public class IfElse {
+    public static void main(String[] args){
+        int grade = 65;
+
+        if (grade > 50) {
+            System.out.println("Great job, you passed!");
+        } else {
+            System.out.println("Study hard next time!");
+        }
+
+        double wallet = 100;
+        double toyCar = 5.99;
+
+        System.out.println("\nCan you buy me this?");
+
+        if (wallet > toyCar) {
+            System.out.println("-Sure!");
+        } else {
+            System.out.println("Sorry, I only have " + wallet + " left");
+        }
+        wallet -= 5.99;
+        // System.out.println(wallet);
+
+        double nike = 95.99;
+        System.out.println("Can I get these nike shoes?");
+        if (wallet > nike) {
+            System.out.println("Sure!");
+        } else {
+            System.out.println("Sorry, there is not enough money left!");
+        }
+    }
+}
