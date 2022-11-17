2c7fd3e85c15745982aa2c4f8cfc3a3f8fb48c70
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-11-16 23:10:30 -0600

    Dealership option

--- Dealership.java
+++ Dealership.java
@@ -7,5 +7,12 @@ public class Dealership {
         System.out.println("Welcome to the Java dealership!");
         System.out.println(" - Select option 'a' to buy a car");
         System.out.println(" - Select option 'b' to sell a car");
+        String option = scan.nextLine();
+        switch (option) {
+            case "a": System.out.println("You chose option " + option); break;
+            case "b": System.out.println("You chose option " + option);break;
+            default: System.out.println("invalid option"); break;
+        }
+        scan.close();
     }
 }
