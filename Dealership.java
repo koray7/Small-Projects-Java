45f764cb980acb1f385fb39d453e973ac3292dde
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-11-16 23:04:36 -0600

    Dealership option

---
+++ Dealership.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Dealership {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Welcome to the Java dealership!");
+        System.out.println(" - Select option 'a' to buy a car");
+        System.out.println(" - Select option 'b' to sell a car");
+    }
+}
