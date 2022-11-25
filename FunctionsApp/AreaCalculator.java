2c8641a879463afc7e7a673731c6b8e6c45b6cc9
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-11-25 11:58:21 -0600

    Area Calculator Rectangle

--- FunctionsApp/AreaCalculator.java
+++ FunctionsApp/AreaCalculator.java
@@ -5,14 +5,21 @@ public class AreaCalculator {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Which calculator do you want to use?");
-        System.out.println("\n1) Area of Square");
+        System.out.println("\nPress '1' for Area of Square");
+        System.out.println("Press '2' for Area of Rectangle");
         int option = scan.nextInt();
 
             if(option == 1) {
-            System.out.println("Enter a number:");
-            double num = scan.nextDouble();
-            double result1 = Math.pow(num, 2);
-            System.out.println("Square of " + num + " is " + result1);
+                System.out.println("Enter the side of the square:");
+                double num = scan.nextDouble();
+                double result = Math.pow(num, 2);
+                System.out.println("Square of " + num + " is " + result);
+            }else if (option == 2){
+                System.out.println("Enter the 'length' and 'width' of Rectangle in same line:");
+                double length = scan.nextDouble();
+                double width = scan.nextDouble();
+                double result = length*width;
+                System.out.println("Are of Rectangle is " + result);
         }
     }
 }
