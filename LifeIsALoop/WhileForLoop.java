e1efb5e7cfedf4a87ab0ca17d14f67518fccac0b
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-12-04 19:30:10 -0600

    Roll Dice, find the doubles

--- LifeIsALoop/WhileForLoop.java
+++ LifeIsALoop/WhileForLoop.java
@@ -25,26 +25,51 @@ public class WhileForLoop{
 
         // ****************************
 
-        System.out.println("\nWelcome to Javagram! Sign in below");
-        System.out.println("-Username: ");
-        String user = scan.nextLine();
-        System.out.println("-Password: ");
-        String pass = scan.nextLine();
+        // System.out.println("\nWelcome to Javagram! Sign in below");
+        // System.out.println("-Username: ");
+        // String user = scan.nextLine();
+        // System.out.println("-Password: ");
+        // String pass = scan.nextLine();
         
         
-        String username = "koray";
-        String password = "java<3";
+        // String username = "koray";
+        // String password = "java<3";
 
-        while (!user.equals(username) || !pass.equals(password)) {
+        // while (!user.equals(username) || !pass.equals(password)) {
         
-            System.out.println("\nIncorrect information, please try again.");
-            System.out.print("- Username: ");
-            user = scan.nextLine();
-            System.out.print("- Password: ");
-            pass = scan.nextLine();
-        }
-        System.out.println("Signed in successfully. WELCOME!");
+        //     System.out.println("\nIncorrect information, please try again.");
+        //     System.out.print("- Username: ");
+        //     user = scan.nextLine();
+        //     System.out.print("- Password: ");
+        //     pass = scan.nextLine();
+        // }
+        // System.out.println("Signed in successfully. WELCOME!");
         
-        scan.close();
+        // scan.close();
+
+        // **********************************
+
+        System.out.println("Let's find doubles.");
+
+        int dice1 = rollDice();
+        int dice2 = rollDice();
+
+        System.out.println("Dice 1: " + dice1);
+        System.out.println("Dice 2: " + dice2);
+
+        while (dice1 != dice2) {
+            dice1 = rollDice();
+            dice2 = rollDice();
+
+            System.out.println("\nDice 1: " + dice1);
+            System.out.println("Dice 2: " + dice2);
         }
+            System.out.println("You rolled doubles.");
+        }
+
+        public static int rollDice() {
+            double randomDecimal = Math.random() * 6;
+            randomDecimal += 1;
+            return (int)randomDecimal;
     }
+}
