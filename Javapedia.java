69ef17307e7b6f6d0774f9c69641880dc8715e02
Koray Kurtoglu <coreykurtoglu@gmail.com>
2023-03-02 17:38:46 -0600

    Creating database

---
+++ Javapedia.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Javapedia {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("\n*******Javapedia*******");
+        System.out.println("How many historical figures will you register?");
+
+        //Users input and creating 2D arrays called "database" with 3 information, depends on users input
+        int people = scan.nextInt();
+        String[][] database = new String[people][3];
+    }
+}
