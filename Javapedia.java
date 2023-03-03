a47a15257b4fd7f531b012cd868b1a52b48d67c1
Koray Kurtoglu <coreykurtoglu@gmail.com>
2023-03-02 18:18:32 -0600

    Created a function that stores the values

--- Javapedia.java
+++ Javapedia.java
@@ -12,12 +12,25 @@ public class Javapedia {
         // Running a loop that stores each information on that row
         scan.nextLine();
         for (int i = 0; i < database.length; i++) {
+            System.out.println("\n\tFigure: " + (i+1));
             System.out.print("\t -Name: ");
             database[i][0] = scan.nextLine();
             System.out.print("\t -Date of birth: ");
             database[i][1] = scan.nextLine();
             System.out.print("\t -Occupation: ");
             database[i][2] = scan.nextLine();
+            System.out.print("\n");
+        }
+        System.out.println("These are the values you stored: ");
+        print2DArray(database);
+        System.out.println("\nWho do you want information on? ");
+    }
+        // Created a function that stores the values
+    public static void print2DArray(String[][] array){
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j]);
+            }
             System.out.print("\n");
         }
     }
